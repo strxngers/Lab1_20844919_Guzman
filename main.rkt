a0ca4b06962807691496747209f9e7c997d0f24c
Elena Guzmán <ivania.guzman@usach.cl>
2021-11-20 13:14:03 -0300

    Main - Create
    Se arregla la función Create, ahora funciona.

--- main.rkt
+++ main.rkt
@@ -6,6 +6,7 @@
 (require "TDAParadigmaDocs.rkt")
 (require "TDAUser.rkt")
 (require "TDADocumento.rkt")
+(require "TDAAccess.rkt")
 
 
 ; FUNCIÓN REGISTER
@@ -20,7 +21,7 @@
 
 ; FUNCIÓN LOGIN
 ; Descripción: función que loguea a un usuario.
-; Tipo de Recursión: 
+; Tipo de Recursión: una de las funciones utilizadas en la función utiliza recursión natural.
 ; Dominio:
 ; Recorrido:
 (define(login paradigmadocs username password operation)
@@ -29,15 +30,18 @@
      (operation paradigmadocs)))
 
 
+
 ; FUNCIÓN CREATE
 ; Descripción:
 ; Tipo de Recursión: 
 ; Dominio: paradigmadocs X date X String (nombreDoc) X String  (contenido)
 ; Recorrido:
 (define create (lambda (paradigmadocs)
-                 (lambda (fecha nombreDoc contenido) paradigmadocs ; aquí hay que empezar a escribir 
-                 ))) ;temporal
-
+                 (lambda (fecha nameDoc content) paradigmadocs
+                   (if (empty? (getListaUserActivo paradigmadocs))
+                       paradigmadocs
+                       (setDoc paradigmadocs (setOwner (createDoc fecha nameDoc content) (car(getListaUserActivo paradigmadocs)))) 
+                 )))) 
 
 
 
@@ -50,8 +54,6 @@
 ; Ejemplo en el que hay dos usuarios iguales, entonces solo registra los que son distintos.
 (define gDocsR2(register (register (register emptyGDocs (fecha 25 10 2021) "user1" "pass1")(fecha 25 10 2021) "user1" "pass2") (fecha 25 10 2021) "user3" "pass3"))
 
-
-
 ; ----------------------------------------------------------- EJEMPLOS FUNCIÓN LOGIN ------------------------------------------------------------------
 
 (define ejemploL1((login ejemploR1 "userR" "passR" create)(fecha 22 02 2021) "xd" "ñe"))
@@ -61,4 +63,21 @@
 ; Ejemplo en el que el usuario no está registrado, entonces no lo loguea ni funciona el create.
 (define gDocsL3 ((login gDocsR2 "user2" "pass2" create) (fecha 30 08 2021) "doc2" "contenido doc2"))
 
+; ----------------------------------------------------------- EJEMPLOS FUNCIÓN CREATE ------------------------------------------------------------------
+(define yay((create ejemploR1) (fecha 19 11 2021) "felicidad" "alegría")) ; primer ejemplo de cuando me finalmente me funcionó create 
+
+
+
+
+; ------------------------------------------------------------ EJEMPLOS FUNCIÓN SHARE ------------------------------------------------------------------
+;Funciones de prueba
+(define gDocs1 (register (register (register emptyGDocs
+                               (fecha 25 10 2021)"user1" "pass1")
+                     (fecha 25 10 2021) "user2" "pass2")
+           (fecha 25 10 2021) "user3" "pass3"))
+(define gDocs2 ((login gDocs1 "user1" "pass1" create) (fecha 30 08 2021) "doc0" "contenido doc0"))
+(define gDocs3 ((login gDocs2 "user1" "pass1" create) (fecha 30 08 2021) "doc1" "contenido doc1"))
+(define gDocs4 ((login gDocs3 "user2" "pass2" create) (fecha 30 08 2021) "doc2" "contenido doc2"))
+(define gDocs5 ((login gDocs4 "user3" "pass3" create) (fecha 30 08 2021) "doc3" "contenido doc3"))
+(define gDocs6 ((login gDocs5 "user1" "pass1" share) 1 (access "user2" #\r)))
 
